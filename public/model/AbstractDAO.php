8291496ad8d3d3f66915b5881ca4cd8fd2f9cff1
Yanislav Vejdarski <yanislav.vejdarski@scalefocus.com>
2020-03-18 16:01:48 +0200

    YV-S11-FP-4-AbstractDao-class In Progress

---
+++ public/model/AbstractDAO.php
@@ -0,0 +1,113 @@
+<?php
+namespace model;
+
+use model\DBManager;
+use PDO;
+
+class AbstractDAO{
+    /**
+     * @var instance
+     */
+   public $pdo;
+
+    /**
+     * AbstractDAO constructor.
+     */
+   public function __construct()
+   {
+       $this->pdo = DBManager::getInstance()->getPDO();
+   }
+
+    /**
+     * @param $sql string
+     * @param null | mixed
+     *
+     * @return bool|PDOStatement
+     */
+    public function prepareAndExecute(
+        $sql,
+        $params = []
+    ) {
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute($params);
+        return $statement;
+    }
+
+    /**
+     * @param $sql string
+     * @param $fetchWay string
+     * @param $params mixed
+     *
+     * @return array
+     */
+   public function fetchAll(
+       $sql,
+       $fetchWay,
+       $params = []
+   ) {
+       $statement = $this->prepareAndExecute(
+           $sql,
+           $params
+       );
+       if ($fetchWay == "fetchAssoc") {
+
+           return $statement->fetchAll(PDO::FETCH_ASSOC);
+       }
+       elseif ($fetchWay == "fetchObject") {
+
+       return $statement->fetchAll(PDO::FETCH_OBJ);
+       }
+   }
+   public function fetchOne(
+       $sql,
+       $fetchWay,
+       $params = []
+
+   ) {
+       $statement = $this->prepareAndExecute(
+           $sql,
+           $params
+       );
+       if ($fetchWay == "fetchAssoc") {
+
+           return $statement->fetch(PDO::FETCH_ASSOC);
+       }
+       elseif ($fetchWay == "fetchObject") {
+
+           return $statement->fetch(PDO::FETCH_OBJ);
+       }
+   }
+
+    /**
+     * @return int
+     */
+    public function lastInsertId()
+    {
+
+       return $this->pdo->lastInsertId();
+    }
+
+    /**
+     *  Rollback
+     */
+    public function rollback()
+    {
+        $this->pdo->rollBack();
+    }
+
+    /**
+     * Begin Transaction
+     */
+    public function beginTransaction()
+    {
+        $this->pdo->beginTransaction();
+    }
+
+    /**
+     * Commit
+     */
+    public function commit()
+    {
+        $this->pdo->commit();
+    }
+}
