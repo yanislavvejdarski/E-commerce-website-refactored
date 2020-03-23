9c6aa8040504915ae168f5492bfd04a068a0234c
Yanislav Vejdarski <yanislav.vejdarski@scalefocus.com>
2020-03-23 09:35:23 +0200

    YV-S11-FP-5-Implementing-AbstractDao-Class Implemented + added Coding Standarts

--- public/model/Address.php
+++ public/model/Address.php
@@ -8,6 +8,12 @@ private $userId;
 private $cityId;
 private $streetName;
 
+    /**
+     * Address constructor.
+     * @param int $userId
+     * @param int $cityId
+     * @param string $streetName
+     */
 public function __construct($userId,$cityId,$streetName)
 {
     $this->userId = $userId;
@@ -15,28 +21,47 @@ public function __construct($userId,$cityId,$streetName)
     $this->streetName = $streetName;
 }
 
+    /**
+     * @param int $id
+     */
     public function setId($id)
     {
         $this->id = $id;
     }
+
+    /**
+     * @return int
+     */
     public function getId()
     {
+
         return $this->id;
     }
 
+    /**
+     * @return int
+     */
     public function getCityId()
     {
+
         return $this->cityId;
     }
 
+    /**
+     * @return string
+     */
     public function getStreetName()
     {
+
         return $this->streetName;
     }
 
-
+    /**
+     * @return int
+     */
     public function getUserId()
     {
+
         return $this->userId;
     }
 }
